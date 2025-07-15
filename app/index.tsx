7ca0983bfe3f33e84aa2ca9da4b0c5466adb2f0a
Vinayak Arora <vinayakarora7461@gmail.com>
2025-07-15 13:49:09 +0530

    feat: add CartButton component and integrate it into the index view

--- app/index.tsx
+++ app/index.tsx
@@ -11,6 +11,7 @@ import {
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../constants/index";
+import CartButton from "@/components/CartButton";
 
 export default function Index() {
   return (
@@ -76,7 +77,7 @@ export default function Index() {
                 />
               </TouchableOpacity>
             </View>
-            <Text>Cart</Text>
+            <CartButton/>
           </View>
         )}
       />
--- components/CartButton.tsx
+++ components/CartButton.tsx
@@ -1,11 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
+import cl from 'clsx';
+import { images } from '@/constants';
 
 const CartButton = () => {
+    const totalItems = 10;
   return (
-    <View>
-      <Text>CartButton</Text>
-    </View>
+    <TouchableOpacity className='cart-btn'onPress={()=>{}}>
+        <Image source={images.bag} className='size-5' resizeMode='contain' />
+        {totalItems > 0 &&(
+            <View className='cart-badge'>
+                <Text className='small-bold text-white'>{totalItems}</Text>
+            </View>
+        )}
+      
+ </TouchableOpacity>
   )
 }
 
