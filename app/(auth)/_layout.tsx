442172b4d0f2019a9bbb770f3b77e7fc345947a5
Vinayak Arora <vinayakarora7461@gmail.com>
2025-07-15 18:55:33 +0530

    feat: refactor authentication layout to use KeyboardAvoidingView and ScrollView

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,12 +1,17 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, KeyboardAvoidingView, Platform, ScrollView, Dimensions } from 'react-native'
 import React from 'react'
 import { Slot } from 'expo-router'
+import cl from 'clsx';
 
 export default function _Layout() {
   return (
-    <SafeAreaView>
-      <Text>Auth_layout</Text>
+<KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' :'height'}>
+     <ScrollView className='bg-white h-full' keyboardShouldPersistTaps='handled'>
+        <View className='w-full relative' style={{height:Dimensions.get('screen').height/2.5}}>
+
+        </View>
+     </ScrollView>
       <Slot />
-    </SafeAreaView>
+    </KeyboardAvoidingView>
   )
 }
